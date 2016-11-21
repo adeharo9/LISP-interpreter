63e85348f474651b7fa4e4f1733a72d391a3260c
Alejandro de Haro <a.deharo9@gmail.com>
2016-11-21 15:45:17 +0100

    Cambios pre-post
    Actualizació pre-post main
    Añadido FunctionsIO.hh
    Añadido pre-post main a doxygen

---
+++ Clases/FunctionsIO.hh
@@ -0,0 +1,38 @@
+/** @file FunctionsIO.hh
+    @brief Especificación de las operaciones de lectura/escritura
+    de expresiones
+*/
+
+#ifndef NO_DIAGRAM
+
+#include <iostream>
+#include "Environment.hh"
+#include "Expression.hh"
+
+#endif
+
+using namespace std;
+
+/** @brief Operación de separación de strings
+
+	\pre <em>Cierto</em>
+	\post 'str' es un string contenido entre espacios o paréntesis en la
+		 entrada que se ha leído
+*/
+void getString(string& str);
+
+/** @brief Operación de lectura de expresiones
+
+	\pre 'env' es un entorno con un espacio de operaciones primitivas inicializado con las operaciones primitivas predefinidas;
+		'exp' es una expresión vacía
+	\post Se han leído los datos del canal estándar de entrada y se han asignado a 'env' si eran una definición de una variable o operación o a 'exp' si representaban una expresión evaluable
+*/
+bool readExpression(Environment& env, Expression& exp);
+
+/** @brief Operación de escritura de expresiones
+
+	\pre 'env' es un entorno con un espacio de operaciones primitivas inicializado con las operaciones primitivas predefinidas;
+		'exp' es una expresión no vacía
+	\post Escribe el valor de la expresión representada por 'exp' por el canal estándar de salida
+*/
+void writeExpression(const Environment& env, const Expression& exp);
--- Clases/main.cc
+++ Clases/main.cc
@@ -1,21 +1,37 @@
+/**
+	* @mainpage PRO2 - Práctica QT16-17: Calculadora de expresiones aritméticas
+ 
+	Este programa constituye una calculadora de expresiones aritméticas usando un lenguaje de tipo LISP
+
+	Se introducen las clases <em>Environment</em>, <em>Expression.hh</em>,
+	<em>PrimitiveOperationSpace</em>, <em>VariableSpace</em>,
+	<em>OperationSpace</em> y <em>Expression</em>
+*/
+
+/** @file main.cc
+    @brief Programa principal para la calculadora de expresiones aritméticas
+*/
+
 #include <iostream>
 #include "Environment.hh"
 #include "Expression.hh"
 
+#ifndef NO_DIAGRAM
+
+#include "FunctionsIO.hh"
+
+#endif
+
 using namespace std;
 
-bool readExpression(Environment& env,Expression& exp){
-
-}
+/** @brief Programa principal para la calculadora de expresiones aritméticas
+*/
 
 int main(){
 	Environment env;
 	Expression exp;
 	while(readExpression(env,exp)){
-		if(not exp.empty()){
-			exp.write();
-			exp.clear();
-		}
+		writeExpression(env,exp);
 	}
 	env.write_env();
 }
