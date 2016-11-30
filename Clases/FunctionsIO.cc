01c3002df123c3d6581c9bc3c6336f9f2cfe325e
adeharo9 <a.deharo9@gmail.com>
2016-11-30 18:15:55 +0100

    Añadidas operaciones de lectura y comprovación de números

--- Clases/FunctionsIO.cc
+++ Clases/FunctionsIO.cc
@@ -1,12 +1,49 @@
 #include <iostream>
+#include <string>
 #include "FunctionsIO.hh"
 #include "Environment.hh"
 #include "Expression.hh"
 
 using namespace std;
 
-void getString(string& str) {
+bool isNum(string str) {
+	int i = 0;
+	if(not str.empty() and (str[i] == '-' or (str[i] >= '0' and str[i] <= '9'))) {
+			++i;
+		while(i < str.size() and str[i] >= '0' and str[i] <= '9') {
+			++i;
+		}
+	}
+	return not str.empty() and i == str.size();
+}
 
+void getString(string& buff, string& str) {
+	if(buff.empty()) {
+		cin >> buff;
+		getString(buff, str);
+	}
+	else if(buff[0] == '(') {
+		str = buff[0];
+		buff.erase(0,1);
+	}
+	else if(buff[0] == ')'){
+		str = buff[0];
+		buff.erase(0,1);
+	}
+	else if(buff[0] == ' ') {
+		buff.erase(0,1);
+		getString(buff, str);
+	}
+	else if(buff == "****") {
+		buff.swap(str);
+	}
+	else {
+		str.clear();
+		while(not buff.empty() and buff[0] != '(' and buff[0] != ')' and buff[0] != ' ') {
+			str+=buff[0];
+			buff.erase(0,1);
+		}
+	}
 }
 
 bool readExpression(Environment& env, Expression& exp) {
