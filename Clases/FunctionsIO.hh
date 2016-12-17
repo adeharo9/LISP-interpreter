7b543a188a71a626fd3dd180e018696e4e283aa7
sergio8215 <sergio_1582@yahoo.es>
2016-12-17 19:31:23 +0100

    Edicion de las pre y post de los metodos faltantes

--- Clases/FunctionsIO.hh
+++ Clases/FunctionsIO.hh
@@ -16,10 +16,25 @@ void getString(string& str);
 
 /** @brief Operación de lectura de expresiones
 
+
 	\pre 'env' es un entorno con un espacio de operaciones primitivas inicializado con las operaciones primitivas predefinidas; 'exp' es una expresión vacía
 	\post Se han leído los datos del canal estándar de entrada y se han asignado a 'env' si eran una definición de una variable o operación o a 'exp' si representaban una expresión evaluable
 */
-bool readExpression(Environment& env, Expression& exp);
+bool read(Environment& env, Expression& exp);
+
+/** @brief Operación para identicar un número en una cadena de strings
+
+	\pre <em>Cierto</em>
+	\post Devuelve cierto si la variable 'str' es un número, falso en caso contrario.
+*/
+bool isNum(string str);
+
+/** @brief Operación de lectura de expresiones
+
+	\pre 'env' es un entorno con un espacio de operaciones primitivas inicializado con las operaciones primitivas predefinidas; 'exp' es una expresión vacía, buff es la expresion de entrada a tratar.
+	\post Se han leído los datos del buff y se han asignado a 'env' si eran una definición de una variable o operación o a 'exp' si representaban una expresión evaluable
+*/
+bool readExpression(Environment& env, Expression& exp, string buff);
 
 /** @brief Operación de escritura de expresiones
 
@@ -27,3 +42,18 @@ bool readExpression(Environment& env, Expression& exp);
 	\post Escribe el valor de la expresión representada por 'exp' por el canal estándar de salida
 */
 void writeExpression(const Environment& env, const Expression& exp);
+
+/** @brief 
+
+	\pre 'env' es un entorno con un espacio de operaciones primitivas inicializado con las operaciones primitivas predefinidas; 'Ininstrn' es una cadena con los valores a definir, puede ser la definicion de una variable o de una operación.
+
+	\post Se ha creado una expresion con una la varible u operación a crear. Si la expresion es correcta, si es indefinida se creara una exp con el valor indefinido.
+*/
+void define(string Ininstrn, Environment& env){
+
+/** @brief 
+
+	\pre 'env' es un entorno con un espacio de operaciones primitivas inicializado con las operaciones primitivas predefinidas; 'exp' es una expresión a evaluar.
+	\post Se evalua la exp y se retorna en la misma variable el valor resultante.
+*/
+void evaluate(Expression& exp, Environment& env);
